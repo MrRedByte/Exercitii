fa656fa3c0fcfe4a3c95039810ab78c8368e7f73
MrRedByte <claudiuu.alex@gmail.com>
2023-09-30 13:01:40 +0300

    Completed fstream version of squareMatrixSearch

--- Matrici/Probleme/Matrix Square Search/littleSquareMatrixMaxFstream.cpp
+++ Matrici/Probleme/Matrix Square Search/littleSquareMatrixMaxFstream.cpp
@@ -1,15 +1,19 @@
-#include<iostream>
+#include<fstream>
 
 int main(){
 	int m, n, v[21][21], maxSum = 0;
-	std::cin >> m >> n;
+	
+	std::ifstream inputFile("file.in");
+	std::ofstream outputFile("file.out");
+
+	inputFile >> m >> n;
 
 	if(2 >= m && m >= 20 && 2 >= n && n >= 20) return 0; 
 	
 	for(int i = 0; i < m; i++){
 		for(int j = 0; j < n; j++){
 			int element;
-			std::cin >> element;
+			inputFile >> element;
 			if(0 > element || element > 1000) return 0;
 			v[i][j] = element;
 		}
@@ -21,7 +25,10 @@ int main(){
 				maxSum = v[i][j] + v[i+1][j] + v[i][j+1] + v[i+1][j+1];
 		}
 	}
-	std::cout << maxSum << '\n';
+	outputFile << maxSum << '\n';
+
+	inputFile.close();
+	outputFile.close();
 	return 0;
 }
 
